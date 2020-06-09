eaf257246adf527c7de61764f70847925aa1e5ca
Simsaris <55785910+Simsaris@users.noreply.github.com>
2020-06-09 03:38:37 +0100

    added HTML form structure with input types, added JS to d:\web-tech-CDA404\CDA404-Coleman-A1\website\contact-page.html

---
+++ website/main.js
@@ -0,0 +1,10 @@
+// form accept message
+function CFaccept() {
+    var checkbox = document.getElementById("CF_accept");
+    var text = document.getElementById("CF_message")
+    if (CF_accept.checked == true) {
+        text.style.display = "block";
+    } else {
+        text.style.display = "none";
+    }
+}
